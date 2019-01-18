e68d77f4a66cbb84c0638ef158d643d244f3c7db
Seth.Wang <seth.wang@keyrus.com>
2019-01-18 14:15:42 +0800

    Revert "remove diccontroller"
    This reverts commit 0e51edefe0840e2af5a1d26459a064d42352559e.

--- basic-service/basic-rest/src/main/java/com/mbb/basic/rest/controller/DictionaryController.java
+++ basic-service/basic-rest/src/main/java/com/mbb/basic/rest/controller/DictionaryController.java
@@ -1,29 +1,45 @@
-package com.mbb.dictionary.rest.controller;
+package com.mbb.basic.rest.controller;
+
 
 import com.mbb.basic.biz.dto.DictionaryInfoDto;
 import com.mbb.basic.biz.dto.DictionaryInfoResponse;
 import com.mbb.basic.biz.dto.DictionaryQueryDto;
+import com.mbb.basic.biz.model.DictionaryModel;
 import com.mbb.basic.biz.service.DictionaryService;
-import com.mbb.basic.rest.controller.BaseController;
+import com.mbb.basic.common.dto.DictValueData;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.mbb.dictionaryvalue.biz.model.DictionaryValueModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.beans.BeanCopier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-/**
- * ${DESCRIPTION}
- *
- * @author lf
- * @create 2019-01-08 15:33
- */
 @RestController
-@RequestMapping("api/v1/dictionary")
-public class DictionaryController extends BaseController {
+@RequestMapping("/api/v1/dict")
+public class DictionaryController {
 
     @Autowired
     private DictionaryService dictionaryService;
 
+    /**
+     * 根据Type获取枚举值
+     * @param type 枚举类型字符串
+     * @return 枚举值List
+     */
+    @GetMapping("/values")
+    public ResponseEntity values(@RequestParam String type){
+
+        List<DictionaryModel> values = dictionaryService.findDictValues(type);
+        List<DictValueData> result = values.stream().map(value -> {
+            DictValueData dictValueData = new DictValueData();
+            BeanCopier.create(DictionaryValueModel.class, DictValueData.class, false)
+                    .copy(value, dictValueData, null);
+            return dictValueData;
+        }).collect(Collectors.toList());
+        return ResponseEntity.ok(result);
+    }
 
     @GetMapping("/info")
     public ResponseEntity getDictionarys(@RequestBody DictionaryQueryDto dictionaryQueryDto) {
