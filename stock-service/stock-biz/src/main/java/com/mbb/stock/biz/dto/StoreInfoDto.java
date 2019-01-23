4d9e4fb5e639d5599a2d178aced07dc87824781c
suc <suc@liby.com>
2019-01-23 17:31:38 +0800

    代码格式化

--- stock-service/stock-biz/src/main/java/com/mbb/stock/biz/dto/StoreInfoDto.java
+++ stock-service/stock-biz/src/main/java/com/mbb/stock/biz/dto/StoreInfoDto.java
@@ -1,19 +1,17 @@
 package com.mbb.stock.biz.dto;
 import java.util.List;
+import lombok.Data;
+
+@Data
 public class StoreInfoDto {
     private String name;
     private Long address;
-
     private List<String> paddress;
     private String detailaddress;
-
-    private String  pstatus;
+    private String pstatus;
     private Long status;
     private String contact;
     private String code;
     private String owner;
     private Long classification;
-
-
-
 }
