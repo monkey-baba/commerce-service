5637fd396641898a2858f51f9dbbd1577a335e2c
ZQian <qian.zhang@keyrus.com>
2019-01-23 14:50:30 +0800

    修改枚举值外部接口，总是获取激活的枚举值

--- basic-service/basic-api/src/main/java/com/mbb/basic/api/DictValueApi.java
+++ basic-service/basic-api/src/main/java/com/mbb/basic/api/DictValueApi.java
@@ -19,92 +19,92 @@ public interface DictValueApi {
      * @return result
      */
     @GetMapping("/api/v1/dict/values")
-    List<DictValueData> getDictValues(@RequestParam("type") String type);
+    List<DictValueData> getDictValues(@RequestParam("type") String type,@RequestParam("active") Boolean active);
 
     @GetMapping("/api/v1/dict/value")
     DictValueData getDictValue(@RequestParam("id")Long id);
 
     default List<DictValueData> getPosType() {
-        return getDictValues(DictionaryType.POS_TYPE.name());
+        return getDictValues(DictionaryType.POS_TYPE.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getChannel() {
-        return getDictValues(DictionaryType.CHANNEL.name());
+        return getDictValues(DictionaryType.CHANNEL.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getProductAttr() {
-        return getDictValues(DictionaryType.PRODUCT_ATTR.name());
+        return getDictValues(DictionaryType.PRODUCT_ATTR.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getSkuSpec() {
-        return getDictValues(DictionaryType.SKU_SPEC.name());
+        return getDictValues(DictionaryType.SKU_SPEC.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getUnit() {
-        return getDictValues(DictionaryType.UNIT.name());
+        return getDictValues(DictionaryType.UNIT.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getApprovedStatus() {
-        return getDictValues(DictionaryType.APPROVED_STATUS.name());
+        return getDictValues(DictionaryType.APPROVED_STATUS.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getPriceType() {
-        return getDictValues(DictionaryType.PRICE_TYPE.name());
+        return getDictValues(DictionaryType.PRICE_TYPE.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getPosClassify() {
-        return getDictValues(DictionaryType.POS_CLASSIFY.name());
+        return getDictValues(DictionaryType.POS_CLASSIFY.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getPosStatus() {
-        return getDictValues(DictionaryType.POS_STATUS.name());
+        return getDictValues(DictionaryType.POS_STATUS.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getBaseStore() {
-        return getDictValues(DictionaryType.BASESTORE.name());
+        return getDictValues(DictionaryType.BASESTORE.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getOrderStatus() {
-        return getDictValues(DictionaryType.ORDER_STATUS.name());
+        return getDictValues(DictionaryType.ORDER_STATUS.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getOrderType() {
-        return getDictValues(DictionaryType.ORDER_TYPE.name());
+        return getDictValues(DictionaryType.ORDER_TYPE.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getPlatform() {
-        return getDictValues(DictionaryType.PLATFORM.name());
+        return getDictValues(DictionaryType.PLATFORM.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getOrderSource() {
-        return getDictValues(DictionaryType.ORDER_SOURCE.name());
+        return getDictValues(DictionaryType.ORDER_SOURCE.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getDeliveryType() {
-        return getDictValues(DictionaryType.DELIVERY_TYPE.name());
+        return getDictValues(DictionaryType.DELIVERY_TYPE.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getCarrier() {
-        return getDictValues(DictionaryType.CARRIER.name());
+        return getDictValues(DictionaryType.CARRIER.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getInvoiceType() {
-        return getDictValues(DictionaryType.INVOICE_TYPE.name());
+        return getDictValues(DictionaryType.INVOICE_TYPE.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getPaymentType() {
-        return getDictValues(DictionaryType.PAYMENT_TYPE.name());
+        return getDictValues(DictionaryType.PAYMENT_TYPE.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getRouterRuleType() {
-        return getDictValues(DictionaryType.ROUTER_RULE_TYPE.name());
+        return getDictValues(DictionaryType.ROUTER_RULE_TYPE.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getConsignmentStatus() {
-        return getDictValues(DictionaryType.CONSIGNMENT_STATUS.name());
+        return getDictValues(DictionaryType.CONSIGNMENT_STATUS.name(), Boolean.TRUE);
     }
 
     default List<DictValueData> getCustomerStatus() {
-        return getDictValues(DictionaryType.CUS_STATUS.name());
+        return getDictValues(DictionaryType.CUS_STATUS.name(), Boolean.TRUE);
     }
 }
